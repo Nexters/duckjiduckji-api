8b0b1c1c575803b3aae32c2ea960f9203f31053a
phantasmicmeans <phantasmicmeans@kakao.com>
2022-01-16 01:39:15 +0900

    init

---
+++ src/main/java/com/nexters/duckji/DuckjiApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.duckji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckjiApplication.class, args);
+	}
+
+}
